e34bc6ae8095ceacb176905c06fccc733663dadc
Mert Saraç <mertsarac@yandex.com>
2020-12-19 13:38:34 +0300

    format fix, progress bar added

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from util.spectrogram import Spectrogram
-from util.database import createDatabasePickle
 from util.genre_classifier import *
+from util.database import *
 
 if __name__ == "__main__":
-    #spectrogram = Spectrogram()
+    genre_types = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+    FRAME_SIZE = 2048
+    HOP_SIZE = 512
 
-    #spectrogram.create_database(enableOffset=True)
-    #spectrogram.create_database(enableOffset=False)
+    SAVE_PATH = "database/spectrograms/"
 
-    createDatabasePickle("database/spectrograms/default", "database/pickle_files/")
-    data, labels = load_data("./database/pickle_files/blues_data.pkl")
-    createTestAndTrain(data, labels)
+    DURATION = 5
+
+    spectrogram_5s = Spectrogram(
+        frame_size=FRAME_SIZE,
+        hop_size=HOP_SIZE,
+        genre_types=genre_types,
+        data_duration=DURATION,
+        split_data=True,
+        number_of_data_for_each_gen=100
+    )
+
+    spectrogram_5s.create_database("./database/Data/genres_original/", "./database/spectrograms/")
+
+
+
+    # createDatabasePickle("./database/spectrograms/", "./database/pickles/")
--- util/spectrogram.py
+++ util/spectrogram.py
@@ -2,20 +2,18 @@ import os
 import librosa
 import librosa.display
 import pylab
-
-genre_types = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
-FRAME_SIZE = 2048
-HOP_SIZE = 512
-
-SAVE_PATH = "database/spectrograms/"
-
-OFFSET = 5
+from tqdm.auto import tqdm
 
 class Spectrogram:
-    def __init__(self):
-        pass
+    def __init__(self, frame_size, hop_size, genre_types, data_duration, split_data, number_of_data_for_each_gen):
+        self.frame_size = frame_size
+        self.hop_size = hop_size
+        self.genre_types = genre_types
+        self.data_duration = data_duration
+        self.split_data = split_data
+        self.number_of_data_for_each_gen = number_of_data_for_each_gen
 
-    def create_database(self, enableOffset=False):
+    def create_database(self, data_path, save_path):
         index_list = []
 
         # get the index list
@@ -25,21 +23,20 @@ class Spectrogram:
             else:
                 index = str(i)
             index_list.append(index)
-        print('Index list created.')
+        print("Split data: ", self.split_data, " Duration: ", self.data_duration)
 
-        print("EnableOffset: ", enableOffset, "\nOffset: ", OFFSET)
-
-        if enableOffset:
-            path = SAVE_PATH + 'enableOffset'
+        # If split_data enable
+        if self.split_data:
+            path = save_path + 'enableOffset/'
 
             # create folder
             if not os.path.exists(path):
                 os.makedirs(path)
 
             # for each gen_type
-            for gen_type in genre_types:
-                print('Gen type: ', gen_type, ' started.')
-
+            for gen_type in self.genre_types:
+                pbar = tqdm(total=self.number_of_data_for_each_gen, position=0, leave=True)
+                pbar.set_description('Gen type: ' + gen_type + ' started')
                 # create folder along with its gen_type
                 if not os.path.exists(path + gen_type):
                     os.makedirs(path + gen_type)
@@ -47,62 +44,65 @@ class Spectrogram:
                 # for each song
                 for index in index_list:
                     # create path for the song
-                    url = "database/Data/genres_original/" + gen_type + "/" + gen_type + '.000' + index + '.wav'
-                    for i in range(0, 30, OFFSET):
+                    url = data_path + gen_type + "/" + gen_type + '.000' + index + '.wav'
+                    for i in range(0, 30, self.data_duration):
                         if not os.path.exists(
                                 path + gen_type + '/' + gen_type + '.000' + index + '_sec' + str(i) + '.png'):
 
                             try:
-                                scale, sr = self.load(url, i, OFFSET)
+                                scale, sr = self.load(url, self.data_duration, i)
                                 S_scale, Y_scale = self.create(scale)
                                 self.save(Y_scale, sr,
                                           path + gen_type + '/' + gen_type + '.000' + index + '_sec' + str(i) + '.png')
                             except:
-                                pass
+                                print('Error occured in creating spectrogram.')
 
-                    print("Index: ", index, " created.")
-
+                    #print("Index: ", index, " created.")
+                    pbar.update()
 
         else:
-            path = SAVE_PATH + 'default/'
+            path = save_path + 'default/'
             if not os.path.exists(path):
                 os.makedirs(path)
-            for gen_type in genre_types:
-                print('Gen type: ', gen_type, ' started.')
+            for gen_type in self.genre_types:
+                pbar = tqdm(total=self.number_of_data_for_each_gen, position=0, leave=True)
+                pbar.set_description('Gen type: ' + gen_type + ' started')
                 if not os.path.exists(path + gen_type):
                     os.makedirs(path + gen_type)
 
                 for index in index_list:
-                    if not os.path.exists(path + gen_type + '/' + gen_type + '.000' + index + '_sec' + str(index) + '.png'):
-                        url = "database/Data/genres_original/" + gen_type + "/" + gen_type + '.000' + index + '.wav'
+                    if not os.path.exists(
+                            path + gen_type + '/' + gen_type + '.000' + index + '_sec' + str(index) + '.png'):
+                        url = data_path + gen_type + "/" + gen_type + '.000' + index + '.wav'
 
                         try:
-                            scale, sr = self.load(url)
+                            scale, sr = self.load(url, self.data_duration)
                             S_scale, Y_scale = self.create(scale)
                             self.save(Y_scale, sr, path + gen_type + '/' + gen_type + '.000' + index + '.png')
 
                         except:
                             pass
 
-                    print("Index: ", index, " created.")
+                    #print("Index: ", index, " created.")
+                    pbar.update()
 
     def create(self, scale):
-        S_scale = librosa.stft(scale, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
+        S_scale = librosa.stft(scale, n_fft=self.frame_size, hop_length=self.hop_size)
         Y_scale = librosa.amplitude_to_db(abs(S_scale))
         return S_scale, Y_scale
 
-    def save(self, Y_scale, sr, save_path):
+    def save(self, y_scale, sr, save_path):
         fig = pylab.gcf()
         fig.set_size_inches(2.4, 2.4)
         pylab.axis('off')  # no axis
         pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[])  # Remove the white edge
 
         librosa.display.specshow(
-            Y_scale
+            y_scale
         )
 
         pylab.savefig(save_path, bbox_inches=None, pad_inches=0)
         pylab.close()
 
-    def load(self, url, offset=0, duration=OFFSET):
-        return librosa.load(url, offset=offset, duration=duration)
+    def load(self, url, data_duration, offset=0):
+        return librosa.load(url, offset=offset, duration=data_duration)
